a3119ad8238cb802a51b74c457bffe99f0a6f17c
Daniel <thedaniellopes@gmail.com>
2023-06-06 16:46:05 -0300

    new api in the block

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,40 @@
-const apiKey = "k_1kvhzf72";
-const searchTerm = " ";
+import apiKey from "./config";
 
+const btn = document.getElementById('movie');
 
-function getMovie(){
-    const searchTerm = document.getElementById('search').value;
-    const url = `https://imdb-api.com/en/API/SearchMovie/${apiKey}/${searchTerm}`;
-    axios.get(url)
+   btn.addEventListener('click', () => {
+
+    //pegar termo digitado pelo usuario
+    const searchTerm = document.getElementById('movieInput').value;
+
+    //chamar api
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`)
     .then(response =>{
-        const movie = response.data;
+        //pegar resultados da api
+        const movie = response.data.results;
+        const image_path = "https://image.tmdb.org/t/p/w500/"
 
-        movie.forEach()
-        console.log(data);
+
+        //mostrar no frontend
+        const divResult = document.getElementById('resultado');
+        divResult.innerText = "";
+        movie.forEach(movie => {  
+            const itemElement = document.createElement('div');
+            itemElement.innerHTML = `<div class="card" style="width: 18rem;">
+                <img src="${image_path}${movie.poster_path}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <p class="card-text">${movie.title}</p>
+                    </div>
+                </div>`
+            divResult.appendChild(itemElement);
+        }) 
+
+        console.log(movie);
     })
     .catch(error => {
         console.log(error);
     })
-}
+   })
+   
+    
 
